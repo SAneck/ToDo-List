71f14c3d4509ad0239aba7b601ae4ec9b5891bc1
Усатов Александр <a.usatov@tepts.local>
2024-12-24 15:24:27 +0300

    Fix bags

--- script.js
+++ script.js
@@ -8,31 +8,65 @@ const tasks = []
 function render(){
     mainDiv.innerHTML = ''
     tasks.forEach((task, indexTask) => {
+
     const newTask = document.createElement('div')
+    const container = document.createElement('div')
+    const containerbtn = document.createElement('div')
+    const containerNewBlocks = document.createElement('div')
+
     const editButton = document.createElement('button')
-    var deelteButton = document.createElement('button')
+    const deelteButton = document.createElement('button')
+    editButton.textContent = 'Edit'
+    deelteButton.textContent = 'Del'
+
+    newTask.classList.add('block')
+    container.classList.add('con')
+    containerbtn.classList.add('con2')
+    containerNewBlocks.classList.add('con-newbl')
+
+    newTask.appendChild(container)
+    newTask.appendChild(containerbtn)
+    newTask.appendChild(containerNewBlocks)
+
+    container.textContent = task
+    containerbtn.appendChild(deelteButton)
+    containerbtn.appendChild(editButton)   
+
     editButton.classList.add('confirmb')
     editButton.addEventListener('click', () => {
+
         const newInput = document.createElement('input')
         const newButton = document.createElement('button')
+        newButton.textContent = 'Ent'
+
         newInput.classList.add('new-input')
         newButton.classList.add('new-button')
-        newTask.appendChild(newButton)
+
         newTask.appendChild(newInput)
+        newTask.appendChild(newButton)
+
         newButton.addEventListener('click', () =>{
+            
+            newInput.value == '' ? alert('Введите текст!') :
             tasks.splice(indexTask, 1, newInput.value)
-            console.log(newInput.value)
             render()
         })
+        if(newButton){
+            editButton.remove()
+            deelteButton.remove()
+        }
+
+        containerNewBlocks.appendChild(newInput)
+        containerNewBlocks.appendChild(newButton)
+
     })
+
     deelteButton.classList.add('deleteb')
     deelteButton.addEventListener('click', () => {
         deleteTask(indexTask)
     })
-    newTask.classList.add('block')
-    newTask.textContent = task
-    newTask.appendChild(deelteButton)
-    newTask.appendChild(editButton)
+
+
     mainDiv.appendChild(newTask)
 })
 }
