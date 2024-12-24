65eb2b16c4f9323f8be47057b28df6aa106de43b
Усатов Александр <a.usatov@tepts.local>
2024-12-24 17:01:12 +0300

    delete probels

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const inputTask = document.querySelector('#text-task')
 const addButton = document.querySelector('#add-button')
 const mainDiv = document.querySelector('.main')
 
-
 const tasks = []
 
 function render(){
     mainDiv.innerHTML = ''
     tasks.forEach((task, indexTask) => {
-
+    
     const newTask = document.createElement('div')
     const container = document.createElement('div')
     const containerbtn = document.createElement('div')
@@ -47,7 +46,7 @@ function render(){
 
         newButton.addEventListener('click', () =>{
             
-            newInput.value == '' ? alert('Введите текст!') :
+            newInput.value.trim() == '' ? alert('Введите текст!') :
             tasks.splice(indexTask, 1, newInput.value)
             render()
         })
@@ -65,14 +64,12 @@ function render(){
     deelteButton.addEventListener('click', () => {
         deleteTask(indexTask)
     })
-
-
-    mainDiv.appendChild(newTask)
+        mainDiv.appendChild(newTask)
 })
 }
 
 function addTask(){
-    inputTask.value == '' ? alert("Вы ничего не ввели") :
+    inputTask.value.trim() == '' ? alert("Вы ничего не ввели") :
     tasks.push(inputTask.value)
     console.log(inputTask.value)
     inputTask.value =''
@@ -83,9 +80,6 @@ function deleteTask(indexTask){
     tasks.splice(indexTask, 1)
     render()
 }
-// function editTask(indexTask){
-    
-// }
 
 addButton.addEventListener('click', addTask)
 
